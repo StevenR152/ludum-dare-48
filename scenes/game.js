c034f1375a3c2d1bba42ff4e457d7376adb0b5d4
Steven Rutherford <dev@steven-rutherford.com>
2021-04-24 18:16:49 +1200

    multifloor tiles

--- scenes/game.js
+++ scenes/game.js
@@ -6,11 +6,23 @@ Crafty.defineScene("Game", function() {
 	// var redSquare = Crafty.e('2D, DOM, Color')
 	// 	.attr({x: 0, y: 0, w: 100, h: 100})
 	// 	.color('#F00');
-Crafty.viewport.scale(0.2);
+Crafty.viewport.scale(0.4);
 	var isos = Crafty.diamondIso.init(TILE_WIDTH,TILE_HEIGHT,20,20);
 	for (var y = 0; y < 20; y++) {
 		for (var x = 0; x < 20; x++) {
 			isos.place(Crafty.e('2D, DOM, Color, tile').attr({w:TILE_WIDTH, h:TILE_HEIGHT}),x+10,y,0);
+		}
+	}
+
+	for (var y = 0; y < 5; y++) {
+		for (var x = 0; x < 5; x++) {
+			isos.place(Crafty.e('2D, DOM, Color, tile').attr({w:TILE_WIDTH, h:TILE_HEIGHT}),x+10,y,4);
+		}
+	}
+
+	for (var y = 0; y < 5; y++) {
+		for (var x = 0; x < 5; x++) {
+			isos.place(Crafty.e('2D, DOM, Color, tile').attr({w:TILE_WIDTH, h:TILE_HEIGHT}),x+10,y,5);
 		}
 	}
 });
