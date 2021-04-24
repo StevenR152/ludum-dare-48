0c195ff2613207e621d5fa178f270a6352c9c62b
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-04-24 11:26:25 +0300

    player height broken but moves

--- scenes/game.js
+++ scenes/game.js
@@ -90,9 +90,9 @@ Crafty.defineScene("Game", function() {
 
 	isos.place(player, player.posx+10, player.posy-5, 1);
   Crafty.bind('Movement', function(e) {
-		isos.place(player, (player.posx + e.x + 10), (player.posy + e.y - 5), 1);
+		// if (map[0][player.posx][player.posy] == 1) {
+				isos.place(player, (player.posx + e.x + 10), (player.posy + e.y - 5), 1);
+		// }
 	})
-
-
 
 });
