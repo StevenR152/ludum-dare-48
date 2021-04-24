6657ee9be81181421aff85eede9169d4706356ab
Steven Rutherford <dev@steven-rutherford.com>
2021-04-24 17:55:21 +1200

    tiles

--- assets.js
+++ assets.js
@@ -1,3 +1,16 @@
 
 // 
-var gameAssets = {};
+var gameAssets = {
+	"audio": {
+		"bgAudio" : ["assets/sounds/music.mp3"],
+		"glass" : ["assets/sounds/glass.wav"],
+		"unlock" : ["assets/sounds/unlock.wav"]
+    },
+    "sprites": {
+       "assets/images/tile.png": {
+           "tile" : 194,
+           "tileh": 135,
+           "map": {"tile": [0, 0]}
+       }
+   }
+}
--- scenes/game.js
+++ scenes/game.js
@@ -1,7 +1,13 @@
 Crafty.defineScene("Game", function() {
 	
-	var redSquare = Crafty.e('2D, DOM, Color')
-		.attr({x: 0, y: 0, w: 100, h: 100})
-		.color('#F00');
+	// var redSquare = Crafty.e('2D, DOM, Color')
+	// 	.attr({x: 0, y: 0, w: 100, h: 100})
+	// 	.color('#F00');
 
+	var isos = Crafty.diamondIso.init(194,135,20,20);
+	for (var y = 0; y < 20; y++) {
+		for (var x = 0; x < 20; x++) {
+			isos.place(Crafty.e('2D, DOM, Color, tile').attr({w:194, h:135}),x+2,y-2,0);
+		}
+	}
 });
