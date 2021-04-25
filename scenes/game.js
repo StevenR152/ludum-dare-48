9eabc46b0fa2e5eacfe639a989fe859c103c1ade
Sumia <sumia.017@gmail.com>
2021-04-25 15:08:30 +0100

    Added level

--- scenes/game.js
+++ scenes/game.js
@@ -5,7 +5,8 @@ Crafty.defineScene("Game", function() {
 		.attr({x: GAME_MUSIC_BUTTON_XPOS, y: GAME_MUSIC_BUTTON_YPOS, w: GAME_MUSIC_BUTTON_WIDTH, h: GAME_MUSIC_BUTTON_HEIGHT})
 		.fixedPosition(GAME_MUSIC_BUTTON_XPOS, GAME_MUSIC_BUTTON_YPOS)
 		.initClick(GAME_MUSIC_BUTTON_XPOS, GAME_MUSIC_BUTTON_YPOS, GAME_MUSIC_BUTTON_WIDTH, GAME_MUSIC_BUTTON_HEIGHT)
-		
+	
+
 	var player = Crafty.e('Player');
 	makeCameraTrackEntity(player, 75);
 	Crafty.viewport.scale(0.3);
@@ -80,6 +81,11 @@ Crafty.defineScene("Game", function() {
 		}
 	});
 
+	//current level label
+	Crafty.e("Level")
+		.attr({x: GAME_SCREEN_WIDTH * 2.8, y: GAME_SCREEN_HEIGHT * 0.15, w: 400, h: 100})
+		.fixedPosition(GAME_SCREEN_WIDTH * 2.8, GAME_SCREEN_HEIGHT * 0.15)
+		.text("Level: " + (current_level + 1) + " of " + map.length)
 
 
 });
