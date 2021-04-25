22b7e99abd0af94dc62b3851002f9ff4af7661fb
Steven Rutherford <steve.rutherford12@gmail.com>
2021-04-26 02:14:42 +1200

    remove my dummy spikes.

--- components/controllers/load_level.js
+++ components/controllers/load_level.js
@@ -13,31 +13,31 @@ Crafty.c("LoadLevel", {
 		}
 		isos.place(player, player.posx, player.posy, 1);
 
-		var button = Crafty.e("Button");
-		isos.place(button, player.posx+1, player.posy+1, 0);
-		var tileSpikes1 = Crafty.e("TileSpikes");
-		isos.place(tileSpikes1, player.posx, player.posy, 0);
-		button.attachLink(tileSpikes1, tileSpikes1.disable);
-		var tileSpikes4 = Crafty.e("TileSpikes");
-		isos.place(tileSpikes4, player.posx-1, player.posy, 0);
-		button.attachLink(tileSpikes4, tileSpikes4.disable);		
-		var tileSpikes5 = Crafty.e("TileSpikes");
-		isos.place(tileSpikes5, player.posx-1, player.posy+1, 0);
-		button.attachLink(tileSpikes5, tileSpikes4.disable);
+		// var button = Crafty.e("Button");
+		// isos.place(button, player.posx+1, player.posy+1, 0);
+		// var tileSpikes1 = Crafty.e("TileSpikes");
+		// isos.place(tileSpikes1, player.posx, player.posy, 0);
+		// button.attachLink(tileSpikes1, tileSpikes1.disable);
+		// var tileSpikes4 = Crafty.e("TileSpikes");
+		// isos.place(tileSpikes4, player.posx-1, player.posy, 0);
+		// button.attachLink(tileSpikes4, tileSpikes4.disable);		
+		// var tileSpikes5 = Crafty.e("TileSpikes");
+		// isos.place(tileSpikes5, player.posx-1, player.posy+1, 0);
+		// button.attachLink(tileSpikes5, tileSpikes4.disable);
 
-		var button2 = Crafty.e("Button");
-		isos.place(button2, player.posx+1, player.posy, 0);
-		var tileSpikes2 = Crafty.e("TileSpikes");
-		isos.place(tileSpikes2, player.posx, player.posy+1, 0);
-		button2.attachLink(tileSpikes2, tileSpikes2.disable);
-		button2.attachLink(tileSpikes4, tileSpikes4.enable);
-		var button3 = Crafty.e("Button");
-		isos.place(button3, player.posx+1, player.posy+2, 0);
-		var tileSpikes3 = Crafty.e("TileSpikes");
-		isos.place(tileSpikes3, player.posx, player.posy+2, 0);
-		button3.attachLink(tileSpikes3, tileSpikes3.disable);
-		button3.attachLink(tileSpikes1, tileSpikes1.enable);
-		button3.attachLink(tileSpikes2, tileSpikes2.enable);
+		// var button2 = Crafty.e("Button");
+		// isos.place(button2, player.posx+1, player.posy, 0);
+		// var tileSpikes2 = Crafty.e("TileSpikes");
+		// isos.place(tileSpikes2, player.posx, player.posy+1, 0);
+		// button2.attachLink(tileSpikes2, tileSpikes2.disable);
+		// button2.attachLink(tileSpikes4, tileSpikes4.enable);
+		// var button3 = Crafty.e("Button");
+		// isos.place(button3, player.posx+1, player.posy+2, 0);
+		// var tileSpikes3 = Crafty.e("TileSpikes");
+		// isos.place(tileSpikes3, player.posx, player.posy+2, 0);
+		// button3.attachLink(tileSpikes3, tileSpikes3.disable);
+		// button3.attachLink(tileSpikes1, tileSpikes1.enable);
+		// button3.attachLink(tileSpikes2, tileSpikes2.enable);
 		console.log("player placed" , player.posx, player.posy);
     },
 
