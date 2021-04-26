2c9b20db5f597a0d64dc89692555d464abe3cf7d
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-04-26 18:52:20 +0300

    added pickup sounds

--- components/player.js
+++ components/player.js
@@ -81,18 +81,21 @@ Crafty.c("Player", {
 			if (map[current_level][1][newy][newx] === 38) {
 				// you got a key
 				Crafty.trigger("KeyCollected", {});
+				audioController.playTrack("key", 1, 0.6);
 				has_key = true;
 			}
 
 			if (map[current_level][1][newy][newx] === 39) {
 				// you got a scroll
 				Crafty.trigger("ScrollCollected", {});
+				audioController.playTrack("scroll", 1, 0.6);
 				has_scroll = true;
 			}
 
 			if (map[current_level][1][newy][newx] === 40) {
 				// you got a scroll
 				Crafty.trigger("FoundCat", {});
+				audioController.playTrack("cat", 1, 0.6);
 				has_cat = true;
 			}
 
@@ -122,7 +125,7 @@ Crafty.c("Player", {
       this.posy += e.y;
 			var footstep_sound = Math.floor(Math.random()*footstep_sounds.length);
 			var step = footstep_sounds[footstep_sound];
-			audioController.playTrack(step, 1, 0.05);
+			audioController.playTrack(step, 1, 0.1);
       isos.place(this, (this.posx), (this.posy), 1);
 
       return;
