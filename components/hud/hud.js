f13466cbe0901676d35beeaa4f44a07429b1f594
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2021-04-26 20:48:49 +0300

    added z value to HUD

--- components/hud/hud.js
+++ components/hud/hud.js
@@ -1,14 +1,15 @@
 Crafty.c("HUD", {
      init: function() {
-        this.bind("EnterFrame", this.adjustPosition) 
+        this.bind("EnterFrame", this.adjustPosition)
      },
-    fixedPosition: function(x, y) {
-        this.fixedX = x; this.fixedY = y;
+    fixedPosition: function(x, y, z) {
+        this.fixedX = x; this.fixedY = y; this.fixedZ = z;
         return this;
    },
 
    adjustPosition: function() {
          this.x = this.fixedX - Crafty.viewport.x;
          this.y = this.fixedY - Crafty.viewport.y;
+         this.z = 2000;
    }
 })
